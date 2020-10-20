c98e3f56d8d6a89b7e75ac46f562b11cd884a61c
Billy Fischbach <fps.billy@gmail.com>
2020-10-20 08:40:14 -0700

    updates

--- config/local.js
+++ config/local.js
@@ -1,119 +1,67 @@
 /**
- * This is the main configuration file.
+ * This is a sample file, which MUST be copied to ./local.js to be loaded
  *
- * Note that this will be checked into the repository, so it MUST NOT contain
- * any secrets whatsoever.
+ * You can overwrite the module.exports from ./default.js here.
  *
- *
- * ========================================================
- * DO NOT MAKE CHANGES IN THIS FILE FOR DEPLOYMENT REASONS!
- * ========================================================
- *
- * If you want to change settings, you can overwrite any by copying over
- * ./sample.js to ./local.js and change your settings there.
- *
- *
- * Some configuration options can be given via environment variables as well.
+ * An example: This would overwrite the default configuration of lighthouse.
+ * During the configuration phase, the objects are merged in this order:
+ *   preset > target
+ * This means, that we can overwrite preset data, if we provide a similar
+ * key in the target definition.
+ * In the example, we do not only change the default presets, but also add
+ * a cookie to be sent for one specific target and activate all categories.
  */
-const os = require('os')
-const path = require('path')
 
-const { TimestampedDirectory } = require('../src/receivers/timestamped-directory')
-const { FilePrometheus } = require('../src/receivers/file-prometheus')
-const { NewRelic } = require('../src/receivers/new-relic')
-
-const debug = require('debug')
-
-let localConfig = {}
-try {
-    localConfig = require('./local')
-} catch (e) {
-    if (e.code !== 'MODULE_NOT_FOUND') {
-        debug('LIGHTMON:ERROR')('config/local.js found, but threw exception while importing: ', e.toString())
-        process.exit(1)
+const presets = {
+    'mobile-snail': {
+        preset: 'mobile-snail',
+        // browserOptions - is an options object that is accepted by launch command of puppeteer
+        // https://github.com/puppeteer/puppeteer/blob/v2.0.0/docs/api.md#puppeteerlaunchoptions
+        browserOptions: {
+            headless: true,
+            args: [
+                '--remote-debugging-port=0',
+                '--no-sandbox',
+            ]
+        },
+        onlyCategories: [
+            'performance',
+        ],
+        throttlingMethod: 'devtools'
     }
 }
 
 
+class LogMeIn {
+    constructor(mailAddress, password, url) {
+        this.url = url
+        this.mailAddress = mailAddress
+        this.password = password
+    }
 
-/**
- * These are the default settings, allowing lighthouse to run in docker
- */
-const presetDefault = {
-    browserOptions: {
-        headless: true,
-        args: [
-            '--no-sandbox'
-        ]
-    },
-    onlyCategories: [
-        'performance'
-    ],
-    output: [
-        'html'
-    ]
-}
+    async setup(browser) {
+        const page = await browser.newPage()
+        await page.goto(this.url)
 
-if (process.env.NO_HEADLESS) {
-    presetDefault.browserOptions.headless = false
-}
+        const emailInput = await page.waitForSelector('input#mail')
+        await emailInput.type(this.mailAddress)
+        const passwordInput = await page.waitForSelector('input#password')
+        await passwordInput.type(this.password)
 
+        const signInButtonSelector = 'submit'
+        await page.waitForSelector(signInButtonSelector)
+        await page.click(signInButtonSelector)
 
-/**
- * By default, lighthouse will run through each of these presets for every
- * target url.
- */
-const presets = {
-    'mobile_mid-slow': Object.assign(
-        {},
-        presetDefault,
-        {
-            preset: 'mobile_mid-slow',
-            emulatedFormFactor: 'mobile',
-            throttlingMethod: 'simulate',
-            throttling: {
-                rttMs: 150,
-                throughputKbps: 1638.4,
-                cpuSlowdownMultiplier: 4
-            }
-        }
-    ),
-    'mobile_mid-fast': Object.assign(
-        {},
-        presetDefault,
-        {
-            preset: 'mobile_mid-fast',
-            emulatedFormFactor: 'mobile',
-            throttlingMethod: 'simulate',
-            throttling: {
-                rttMs: 28,
-                throughputKbps: 16000,
-                cpuSlowdownMultiplier: 4
-            }
-        }
-    ),
-    'desktop-fast': Object.assign(
-        {},
-        presetDefault,
-        {
-            preset: 'desktop-fast',
-            emulatedFormFactor: 'desktop',
-            throttlingMethod: 'simulate',
-            throttling: {
-                rttMs: 28,
-                throughputKbps: 16000,
-                cpuSlowdownMultiplier: 4
-            }
-        }
-    )
+        await page.waitForSelector('#dashboard')
+        await page.close()
+    }
 }
 
 
+const loginPrehook = new LogMeIn('email@example.com',
+    'password', 'https://verivox.de/login-url/');
 
-/**
- * These are the targets to run through. The name needs to be unique and will
- * be used in the receivers.
- */
+
 const targets = [
     {
         name: 'openminds',
@@ -141,116 +89,7 @@ const targets = [
     }
 ]
 
-
-const WebserverOptions = {
-    port: process.env.NODE_PORT || 3000,
-    publicFolder: 'public',
+module.exports = {
+    presets,
+    targets
 }
-
-
-/**
- * Build the configuration object with some dynamic receiver insertion
- * depending on environment variables
- */
-const receivers = []
-
-/**
- * The directory where reports are stored.
- * @type {string}
- */
-const reportDir = process.env.REPORT_DIR || localConfig.reportDir || path.join(os.tmpdir(), 'lightmon')
-
-/**
- * The directory, in which the cache for the reports are stored
- * @type {string|*}
- */
-const cacheDir = process.env.CACHE_DIR || localConfig.cacheDir || os.tmpdir()
-
-const prometheusMetricsFile = path.join(WebserverOptions.publicFolder, 'metrics', 'index.html')
-const jsonMetricsFile = path.join(WebserverOptions.publicFolder, 'metrics.json')
-
-
-/**
- * How often do we expect reports? /healthz will return a 500, if no report has been seen in that time
- * defaults to once every 25h
- * @type {number}
- */
-const expectedLastReportInSec = localConfig.expectedLastReportInSec || 25*60*60
-
-
-/**
- * If the reports are saved on a network folder, the filesystem watcher will most likely not work.
- * This instructs chokidar to use polling instead. Set to a non-number to disable polling.
- * @type {number|false}
- */
-const reportsPollingEverySec = false
-
-
-receivers.push(
-    new TimestampedDirectory(reportDir),
-    new FilePrometheus(prometheusMetricsFile)
-)
-
-const newRelicOptions = {
-    accountId: process.env.NEW_RELIC_ACCOUNT_ID,
-    apiKey: process.env.NEW_RELIC_API_KEY
-}
-if (newRelicOptions.accountId || newRelicOptions.apiKey) {
-    receivers.push(
-        new NewRelic(newRelicOptions)
-    )
-}
-
-
-/**
- * Do you want to use a mutex to make sure, that no evaluation will run in parallel?
- *
- * Uncomment the following line for the default linux behavior
- */
-//const lockFile = process.geteuid() === 0 ? '/run/lightmon-evaluate.lock' : `/run/user/${process.geteuid()}/lightmon-evaluate.lock`
-const lockFile = null
-
-/**
- * We user proper-lockfile for process locking - you can add your options here if you want to.
- * @see https://github.com/moxystudio/node-proper-lockfile
- */
-const lockFileOptions = {
-    stale: 60000,
-    update: 5000
-}
-
-
-/**
- * Overwrite this default configuration with the local.js, if present
- */
-const config = Object.assign(
-    {
-        presets,
-        receivers,
-        targets,
-        WebserverOptions,
-        prometheusMetricsFile,
-        jsonMetricsFile,
-        lockFile,
-        lockFileOptions,
-        cacheDir,
-        reportDir,
-        expectedLastReportInSec,
-        reportsPollingEverySec
-    },
-    localConfig
-)
-
-// TODO: this is getting ridiculous - create a real config class
-config.reportDir = reportDir
-
-// el cheapo validity check
-if (!presets || Object.keys(presets).length === 0 ||
-    !receivers || receivers.length === 0 ||
-    !targets || targets.length === 0) {
-    debug('LIGHTMON:ERROR')('Invalid config. I require at least one preset, one receiver and one target')
-    process.exit(1)
-}
-
-// all set
-module.exports = config
